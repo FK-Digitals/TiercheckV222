7d57b7c1361ed5d198257754a63e3101d9852aa9
FK-Digitals <FK-Digitals@outlook.de>
2025-08-09 04:01:21 +0200

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "014da91253c1f997"; }
+/******/ 		__webpack_require__.h = function() { return "384b65ba20410207"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
